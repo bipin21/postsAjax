edf97ba93dadc2246ea623c619ddf730c76f70bd
Bipin Karki <bipinkarki172@email.com>
2022-09-18 15:48:19 -0400

    ajax exercise

---
+++ out/artifacts/postsAjax_war_exploded/index.jsp
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>cs472.mum.Post Application</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="resources/js/posts.js"></script>
+</head>
+<body>
+
+<div>
+    <label for='userid'>Enter a userId</label>
+    <input id='userid' type='text' name='userid'/>
+</div>
+
+<p><input id='send' type='button' value='Send'/></p>
+<p>List of posts</p>
+<div id='postList'></div>
+
+</body>
+</html>
+
+
+
+
+
---
+++ web/resources/js/posts.js
@@ -0,0 +1,66 @@
+$(function () {
+    $("#send").click(getPosts);
+
+    $(document).on('click', '.show-comment', function (e) {
+        let postId = $(this).attr('data-postId');
+        getComments(postId);
+    })
+});
+
+function getPosts() {
+    var userId = $("#userid").val();
+
+    $.ajax("https://jsonplaceholder.typicode.com/posts", {
+        "type": "get",
+        data: {
+            userId: userId
+        },
+    }).done(displayPosts);
+}
+
+function displayPosts(data) {
+    // prepare posts dom
+    var postList = "<div>";
+    data.forEach(function (item) {
+        postList += "<div id='post-" + item.id + "'>ID: " +
+            item.id + "<br/>" +
+            "<strong>Title: " + item.title + "</strong>" + "<br/>"
+            + "Body: " + item.body + "<br/>"
+            + "<button class='show-comment' type='submit' data-postId='" + item.id + "'  > Show Comments </button><br/>" +
+            "</div>";
+    });
+    postList += "<div>";
+    $("#postList").html(postList);
+
+    // clean up input fields
+    $("#userid").val('');
+
+}
+
+
+function getComments(postId) {
+    $.ajax("https://jsonplaceholder.typicode.com/comments", {
+        "type": "get",
+        data: {
+            postId: postId
+        },
+    }).done(displayComments);
+}
+
+function displayComments(data) {
+    // prepare comment dom
+    var postComments = "<div>";
+    data.forEach(function (item) {
+        postComments += "<p>ID: " +
+            item.id + "<br/>" +
+            "<strong>Name: " + item.name + "</strong>" + "<br/>" +
+            "Email: " + item.email + "<br/>"
+            + "Comment: " + item.body + "<br/>" +
+            "</p>";
+    });
+    postComments += "<div>";
+    let postId = data ? data[0].postId : '';
+    let post = $("#postList #post-" + postId).html();
+    $("#postList").html(post);
+    $("#postList").append(postComments);
+}
